bf0896a28b600701a723276f9911178c4c558608
徐期涛 <jeremyxuqitao@outlook.com>
2019-07-25 16:55:25 -0500

    Update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Cache
 It a cache project designed and implemented by Qitao Xu.
+Please download this repo to your own laptop and follow the instruction to run ubit test.
 
 ## Get, Set, GetMultiple, SetMultiple, Delete
 
@@ -14,6 +15,12 @@ It has been added in LRUCache.py file.
 
 ## Python Client
 
+I am not sure how to finish this part, but I did try to search online and find a 
+turtorial: [Python Request](https://realpython.com/python-requests/) written by Alex Ronquillo. 
+And please check in the directory:
+```bash
+python3 client.py
+```
 
 ## Unit Test
 Just download this repo and walk in the directory and run
---
+++ client.py
@@ -0,0 +1,28 @@
+# reference: https://realpython.com/python-requests/
+# author: Alex Ronquillo
+
+import requests
+
+# Search GitHub's repositories for requests
+response = requests.get(
+    'https://api.github.com/search/repositories',
+    params={'q': 'requests+language:python'},
+)
+
+# Inspect some attributes of the `requests` repository
+json_response = response.json()
+repository = json_response['items'][0]
+print(f'Repository name: {repository["name"]}')  # Python 3.6+
+print(f'Repository description: {repository["description"]}')  # Python 3.6+
+
+response = requests.get(
+    'https://api.github.com/search/repositories',
+    params={'q': 'requests+language:python'},
+    headers={'Accept': 'application/vnd.github.v3.text-match+json'},
+)
+
+# View the new `text-matches` array which provides information
+# about your search term within the results
+json_response = response.json()
+repository = json_response['items'][0]
+print(f'Text matches: {repository["text_matches"]}')
